6ab8fa46a2376838435125ccb46bfbd6500b38ac
Aiden Thinn (RIT Student) <mzt5441@ad.rit.edu>
2019-02-07 20:01:31 -0500

    working on it

---
+++ src/jsonResponses.js
@@ -0,0 +1,182 @@
+const respond = (request, response, status, content, type) => {
+  response.writeHead(status, { 'Content-Type': type });
+  response.write(content);
+  response.end();
+};
+
+const success = (request, response, acceptedTypes) => {
+  const responseJSON = {
+    message: 'This is a successful response.',
+    id: 'Success',
+  };
+
+  if (acceptedTypes[0] === 'text/xml') {
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <message> ${responseJSON.message} </message>`;
+    responseXML = `${responseXML} <id> ${responseJSON.id} </id>`;
+    responseXML = `${responseXML} </response>`;
+
+    return respond(request, response, 200, responseXML, 'text/xml');
+  }
+
+  const responseString = JSON.stringify(responseJSON);
+  return respond(request, response, 200, responseString, 'application/json');
+};
+
+const badRequest = (request, response, acceptedTypes, params) => {
+  const responseJSON = {
+    message: 'This request has required parameters.',
+    id: 'Bad Request',
+  };
+
+  if (!params.valid || params.valid !== true) {
+    responseJSON.message = 'Missing valid query parameter set to true.';
+    responseJSON.id = 'badRequest';
+
+    if (acceptedTypes[0] === 'text/xml') {
+      let responseXML = '<response>';
+      responseXML = `${responseXML} <message> ${responseJSON.message} </message>`;
+      responseXML = `${responseXML} <id> ${responseJSON.id} </id>`;
+      responseXML = `${responseXML} </response>`;
+
+      return respond(request, response, 400, responseXML, 'text/xml');
+    }
+
+    const responseString = JSON.stringify(responseJSON);
+    return respond(request, response, 400, responseString, 'application/json');
+  }
+
+  if (acceptedTypes[0] === 'text/xml') {
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <message> ${responseJSON.message} </message>`;
+    responseXML = `${responseXML} <id> ${responseJSON.id} </id>`;
+    responseXML = `${responseXML} </response>`;
+
+    return respond(request, response, 200, responseXML, 'text/xml');
+  }
+
+  const responseString = JSON.stringify(responseJSON);
+  return respond(request, response, 200, responseString, 'application/json');
+};
+
+const unauthorized = (request, response, acceptedTypes, params) => {
+  const responseJSON = {
+    message: 'You have successfully viewed the content.',
+    id: 'Unauthorized',
+  };
+
+  if (!params.valid || params.valid !== true) {
+    responseJSON.message = 'Missing loggedIn query parameter set to yes.';
+    responseJSON.id = 'unauthorized';
+
+    if (acceptedTypes[0] === 'text/xml') {
+      let responseXML = '<response>';
+      responseXML = `${responseXML} <message> ${responseJSON.message} </message>`;
+      responseXML = `${responseXML} <id> ${responseJSON.id} </id>`;
+      responseXML = `${responseXML} </response>`;
+
+      return respond(request, response, 401, responseXML, 'text/xml');
+    }
+
+    const responseString = JSON.stringify(responseJSON);
+    return respond(request, response, 401, responseString, 'application/json');
+  }
+
+  if (acceptedTypes[0] === 'text/xml') {
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <message> ${responseJSON.message} </message>`;
+    responseXML = `${responseXML} <id> ${responseJSON.id} </id>`;
+    responseXML = `${responseXML} </response>`;
+
+    return respond(request, response, 200, responseXML, 'text/xml');
+  }
+
+  const responseString = JSON.stringify(responseJSON);
+  return respond(request, response, 200, responseString, 'application/json');
+};
+
+const forbidden = (request, response, acceptedTypes) => {
+  const responseJSON = {
+    message: 'You do not have access to this content',
+    id: 'Forbidden',
+  };
+
+  if (acceptedTypes[0] === 'text/xml') {
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <message> ${responseJSON.message} </message>`;
+    responseXML = `${responseXML} <id> ${responseJSON.id} </id>`;
+    responseXML = `${responseXML} </response>`;
+
+    return respond(request, response, 403, responseXML, 'text/xml');
+  }
+
+  const responseString = JSON.stringify(responseJSON);
+  return respond(request, response, 403, responseString, 'application/json');
+};
+
+const internal = (request, response, acceptedTypes) => {
+  const responseJSON = {
+    message: 'Internal server error, something went wrong.',
+    id: 'Internal Error',
+  };
+
+  if (acceptedTypes[0] === 'text/xml') {
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <message> ${responseJSON.message} </message>`;
+    responseXML = `${responseXML} <id> ${responseJSON.id} </id>`;
+    responseXML = `${responseXML} </response>`;
+
+    return respond(request, response, 500, responseXML, 'text/xml');
+  }
+
+  const responseString = JSON.stringify(responseJSON);
+  return respond(request, response, 500, responseString, 'application/json');
+};
+
+const notImplemented = (request, response, acceptedTypes) => {
+  const responseJSON = {
+    message: 'A get request for this page has not been implemented yet. Check later for updated content.',
+    id: 'Not Implemented',
+  };
+
+  if (acceptedTypes[0] === 'text/xml') {
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <message> ${responseJSON.message} </message>`;
+    responseXML = `${responseXML} <id> ${responseJSON.id} </id>`;
+    responseXML = `${responseXML} </response>`;
+
+    return respond(request, response, 501, responseXML, 'text/xml');
+  }
+
+  const responseString = JSON.stringify(responseJSON);
+  return respond(request, response, 501, responseString, 'application/json');
+};
+
+const notFound = (request, response, acceptedTypes) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  if (acceptedTypes[0] === 'text/xml') {
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <message> ${responseJSON.message} </message>`;
+    responseXML = `${responseXML} <id> ${responseJSON.id} </id>`;
+    responseXML = `${responseXML} </response>`;
+
+    return respond(request, response, 404, responseXML, 'text/xml');
+  }
+
+  const responseString = JSON.stringify(responseJSON);
+  return respond(request, response, 404, responseString, 'application/json');
+};
+
+module.exports = {
+  success,
+  badRequest,
+  unauthorized,
+  forbidden,
+  internal,
+  notImplemented,
+  notFound,
+};
